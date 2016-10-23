3c12e06611e3076c22dac52a994713a77d1a7f63
Ryan MacLennan <galica9999@gmail.com>
2016-10-23 15:32:51 -0400

    Have figured out to make a grid and add labels to make indivualized sections for each type of command.  I am currentl adding the rest of the commands in the tool kit and will e creating mroe sections for them.  Once I have the check buttons justified, I will start with making the commands actualy run. Then add the functionality to have the check boxes open the options for each command.

--- gui.py
+++ gui.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+#
+#************
+#Author: Eyan MacLennan
+#Filename: gui.py
+#This program is to be a gui application for the Sleuth Kit
+#************
 import tkinter
 import os
 istat = "/bin/istat.exe"
@@ -19,9 +25,9 @@ window.wm_title("Sleuth Kit Visualizer")#sets the title for the "window"
 
 
 #*********************************************************************#
-#								      #
-#Tools Section							      #
-#								      #
+#
+#Tools Section
+#
 #*********************************************************************#
 
 
@@ -32,17 +38,17 @@ c1 = tkinter.Checkbutton(window, text = "Istat", \
 #File system layer tools
 #*****
 
-c2 = tkinter.Checkbutton(window, text = "FSStat", \
+fslt1 = tkinter.Checkbutton(window, text = "FSStat", \
     variable = fsstat, height = 3, width = 20)#creates checkbox for fsstat command 
 
 #*****
 #File name layer tools
 #*****
 
-c3 = tkinter.Checkbutton(window, text = "FLS", \
+fnlt1 = tkinter.Checkbutton(window, text = "FLS", \
     variable = fls, height = 3, width = 20)#creates checkbox for fls command 
 
-c4 = tkinter.Checkbutton(window, text = "FFIND", \
+fnlt2 = tkinter.Checkbutton(window, text = "FFIND", \
     variable = ffind, height = 3, width = 20)#creates checkbox for ffind command 
 
 #*****
@@ -60,17 +66,17 @@ c6 = tkinter.Checkbutton(window, text = "Icat", \
 #Fully automated tools
 #*****
 
-c7 = tkinter.Checkbutton(window, text = "TSK CompareDir", \
+fat1 = tkinter.Checkbutton(window, text = "TSK CompareDir", \
     variable = tsk_comparedir, height = 3, width = 20)#creates checkbox for tsk_comparedir command 
 
-c8 = tkinter.Checkbutton(window, text = "TSK GetTimes", \
+fat2 = tkinter.Checkbutton(window, text = "TSK GetTimes", \
     variable = tsk_gettimes, height = 3, width = 20)#creates checkbox for tsk_gettimes command 
 
 
-c9 = tkinter.Checkbutton(window, text = "TSK Load DB", \
+fat3 = tkinter.Checkbutton(window, text = "TSK Load DB", \
     variable = tsk_loaddb, height = 3, width = 20)#creates checkbox for tsk_loaddb command 
 
-c10 = tkinter.Checkbutton(window, text = "TSK Recover", \
+fat4 = tkinter.Checkbutton(window, text = "TSK Recover", \
     variable = tsk_recover, height = 3, width = 20)#creates checkbox for tsk_recover command 
 
 
@@ -79,24 +85,49 @@ c10 = tkinter.Checkbutton(window, text = "TSK Recover", \
 #Image File Tools
 #*****
 
-c11 = tkinter.Checkbutton(window, text = "IMG Stat", \
+ift1 = tkinter.Checkbutton(window, text = "IMG Stat", \
     variable = img_stat, height = 3, width = 20)#creates checkbox for img_stat command 
 
 
-c12 = tkinter.Checkbutton(window, text = "IMG Cat", \
+ift2 = tkinter.Checkbutton(window, text = "IMG Cat", \
     variable = img_cat, height = 3, width = 20)#creates checkbox for img_cat command 
 
-c1.pack()   #packs the checkbox
-c2.pack()
-c3.pack()
-c4.pack()
-c5.pack()
-c6.pack()
-c7.pack()   #packs the checkbox
-c8.pack()
-c9.pack()
-c10.pack()
-c11.pack()
-c12.pack()
+#*******
+#Making the grid pattern
+#*******
+
+toolsLabel = tkinter.Label(text="Tools", font='Helvetica 14 bold')
+fsltLabel = tkinter.Label(text="File System Layer Tools", font='Helvetica 14 bold')
+fnltLabel = tkinter.Label(text="File Name Layer Tools", font='Helvetica 14 bold')
+blahLabel = tkinter.Label(text="Blah tools", font='Helvetica 14 bold')
+fatLabel = tkinter.Label(text="Fully Automated Tools", font='Helvetica 14 bold')
+iftLabel = tkinter.Label(text="Image File Tools",font='Helvetica 14 bold')
+
+toolsLabel.grid(row=0, columnspan=4)
+
+c1.grid(row=1, column=0, sticky='E')
+
+fsltLabel.grid(row=2, columnspan=4)
+fslt1.grid(row=3, column=0, sticky='E')
+
+fnltLabel.grid(row=4, columnspan=4)
+fnlt1.grid(row=5, column=0, sticky='E')
+fnlt2.grid(row=5, column=1, sticky='E')
+
+blahLabel.grid(row=6, columnspan=4)
+c5.grid(row=7, column=0, sticky='E')
+c6.grid(row=7, column=1,sticky='E')
+
+fatLabel.grid(row=8, columnspan=4)
+fat1.grid(row=9, column=0, sticky='E')
+fat2.grid(row=9, column=1, sticky='E')
+fat3.grid(row=9, column=2, sticky='E')
+fat4.grid(row=9, column=3, sticky='E')
+
+iftLabel.grid(row=10, columnspan=4)
+ift1.grid(row=11, column=0, sticky='E')
+ift2.grid(row=11, column=1, sticky='E')
+
+
 
 window.mainloop()                       #runs the tkinter window
