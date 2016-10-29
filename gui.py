6ac1e33c399e334732481e7cca7de907402e6665
Ryan MacLennan <galica9999@gmail.com>
2016-10-28 21:47:02 -0400

    have finished adding all the commands and I have implemented the next button.  Although it doesn;t go anywhere yet, since a frame instance hasn;t been created for the options page of the commands.  Going to try to get it to dynamically allocate the commands when pressing next.

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
 #************
-#Author: Eyan MacLennan
+#Author: Ryan MacLennan
 #Filename: gui.py
 #This program is to be a gui application for the Sleuth Kit
 #************
@@ -11,7 +11,7 @@ istat = "/bin/istat.exe"
 fsstat = "/bin/fsstat.exe"
 fls = "/bin/fls.exe"
 ffind = "/bin/ffind.exe"
-fcat = "/bin/fcat.ext"
+fcat = "/bin/fcat.exe"
 icat = "/bin/icat.exe"
 tsk_recover = "/bin/tsk_recover.exe"
 tsk_loaddb = "/bin/tsk_loaddb.exe"
@@ -19,6 +19,18 @@ tsk_gettimes = "/bin/tsk_gettimes.exe"
 tsk_comparedir = "/bin/tsk_comparedir.exe"
 img_stat = "/bin/img_stat.exe"
 img_cat = "/bin/img_cat.exe"
+ils = "/bin/ils.exe"
+ifind = "/bin/ifind.exe"
+icat = "/bin/icat.exe"
+blkcat = "/bin/blkcat.exe"
+blkls = "/bin/blkls.exe"
+blkstat = "/bin/blkstat.exe"
+blkcalc = "/bin/blkcalc.exe"
+jcat = "/bin/jcat.exe"
+jls = "/bin/jls.exe"
+mmls = "/bin/mmls.exe"
+mmcat = "/bin/mmcat.exe"
+mmstat = "/bin/mmstat.exe"
 
 window = tkinter.Tk()                   #initialized the window for tkinter
 window.wm_title("Sleuth Kit Visualizer")#sets the title for the "window"
@@ -30,9 +42,6 @@ window.wm_title("Sleuth Kit Visualizer")#sets the title for the "window"
 #
 #*********************************************************************#
 
-
-c1 = tkinter.Checkbutton(window, text = "Istat", \
-    variable = istat, height = 3, width = 20)#creates checkbox for istat command 
 
 #*****
 #File system layer tools
@@ -52,15 +61,21 @@ fnlt2 = tkinter.Checkbutton(window, text = "FFIND", \
     variable = ffind, height = 3, width = 20)#creates checkbox for ffind command 
 
 #*****
-#
+#Meta Data Layer Tools
 #*****
 
-c5 = tkinter.Checkbutton(window, text = "FCat", \
-    variable = fcat, height = 3, width = 20)#creates checkbox for fcat command 
+mdlt1 = tkinter.Checkbutton(window, text = "Icat", \
+    variable = icat, height = 3, width = 20)#creates checkbox for icat command
 
+mdlt2 = tkinter.Checkbutton(window, text = "Istat", \
+    variable = istat, height = 3, width = 20)#creates checkbox for istat command
 
-c6 = tkinter.Checkbutton(window, text = "Icat", \
-    variable = icat, height = 3, width = 20)#creates checkbox for icat command 
+mdlt3 = tkinter.Checkbutton(window, text = "ILS", \
+    variable = ils, height = 3, width = 20)#creates checkbox for ils command
+
+mdlt4 = tkinter.Checkbutton(window, text = "IFind", \
+    variable = ifind, height = 3, width = 20)#creates checkbox for ifind command
+
 
 #*****
 #Fully automated tools
@@ -92,6 +107,47 @@ ift1 = tkinter.Checkbutton(window, text = "IMG Stat", \
 ift2 = tkinter.Checkbutton(window, text = "IMG Cat", \
     variable = img_cat, height = 3, width = 20)#creates checkbox for img_cat command 
 
+
+#*******
+#Data Unit Layer Tools
+#*******
+
+dult1 = tkinter.Checkbutton(window, text = "FCat", \
+    variable = fcat, height = 3, width = 20)#creates checkbox for fcat command
+
+dult2 = tkinter.Checkbutton(window, text = "BLKcat", \
+    variable = blkcat, height = 3, width = 20)#creates checkbox for blkcat command
+
+dult3 = tkinter.Checkbutton(window, text = "BLKls", \
+    variable = blkls, height = 3, width = 20)#creates checkbox for blkls command
+
+dult4 = tkinter.Checkbutton(window, text = "BLKstat", \
+    variable = blkstat, height = 3, width = 20)#creates checkbox for blkstat command
+
+dult5 = tkinter.Checkbutton(window, text = "BLKcalc", \
+    variable = blkcalc, height = 3, width = 20)#creates checkbox for fcat command
+
+#*******
+#File System Journal Tools
+#*******
+fsjt1 = tkinter.Checkbutton(window, text = "JCAT", \
+    variable = jcat, height = 3, width = 20)#creates checkbox for fcat command
+
+fsjt2 = tkinter.Checkbutton(window, text = "JLS", \
+    variable = jls, height = 3, width = 20)#creates checkbox for fcat command
+
+#*******
+#Volume System Tools
+#*******
+vst1 = tkinter.Checkbutton(window, text = "MMLS", \
+    variable = mmls, height = 3, width = 20)#creates checkbox for fcat command
+
+vst2 = tkinter.Checkbutton(window, text = "MMStat", \
+    variable = mmstat, height = 3, width = 20)#creates checkbox for fcat command
+
+vst3 = tkinter.Checkbutton(window, text = "MMCat", \
+    variable = mmcat, height = 3, width = 20)#creates checkbox for fcat command
+
 #*******
 #Making the grid pattern
 #*******
@@ -99,35 +155,58 @@ ift2 = tkinter.Checkbutton(window, text = "IMG Cat", \
 toolsLabel = tkinter.Label(text="Tools", font='Helvetica 14 bold')
 fsltLabel = tkinter.Label(text="File System Layer Tools", font='Helvetica 14 bold')
 fnltLabel = tkinter.Label(text="File Name Layer Tools", font='Helvetica 14 bold')
-blahLabel = tkinter.Label(text="Blah tools", font='Helvetica 14 bold')
+dultLabel = tkinter.Label(text="Data Unit Layer Tools", font='Helvetica 14 bold')
 fatLabel = tkinter.Label(text="Fully Automated Tools", font='Helvetica 14 bold')
 iftLabel = tkinter.Label(text="Image File Tools",font='Helvetica 14 bold')
+mdltLabel = tkinter.Label(text="Meta Data Layer Tools", font='Helvetica 14 bold')
+fsjtLabel = tkinter.Label(text="File System Journal Tools", font='Helvetica 14 bold')
+vstLabel = tkinter.Label(text="Volume System Tools", font = 'Helvetica 14 bold')
 
-toolsLabel.grid(row=0, columnspan=4)
+toolsLabel.grid(row=0, columnspan=5)
 
-c1.grid(row=1, column=0, sticky='E')
+fsltLabel.grid(row=2, columnspan=5)
+fslt1.grid(row=3, column=0)
 
-fsltLabel.grid(row=2, columnspan=4)
-fslt1.grid(row=3, column=0, sticky='E')
+fnltLabel.grid(row=4, columnspan=5)
+fnlt1.grid(row=5, column=0)
+fnlt2.grid(row=5, column=1)
 
-fnltLabel.grid(row=4, columnspan=4)
-fnlt1.grid(row=5, column=0, sticky='E')
-fnlt2.grid(row=5, column=1, sticky='E')
+dultLabel.grid(row=6, columnspan=5)
+dult1.grid(row=7, column=0)
+dult2.grid(row=7, column=1)
+dult3.grid(row=7, column=2)
+dult4.grid(row=7, column=3)
+dult5.grid(row=7, column=4)
 
-blahLabel.grid(row=6, columnspan=4)
-c5.grid(row=7, column=0, sticky='E')
-c6.grid(row=7, column=1,sticky='E')
+fatLabel.grid(row=8, columnspan=5)
+fat1.grid(row=9, column=0)
+fat2.grid(row=9, column=1)
+fat3.grid(row=9, column=2)
+fat4.grid(row=9, column=3)
 
-fatLabel.grid(row=8, columnspan=4)
-fat1.grid(row=9, column=0, sticky='E')
-fat2.grid(row=9, column=1, sticky='E')
-fat3.grid(row=9, column=2, sticky='E')
-fat4.grid(row=9, column=3, sticky='E')
+iftLabel.grid(row=10, columnspan=5)
+ift1.grid(row=11, column=0)
+ift2.grid(row=11, column=1)
 
-iftLabel.grid(row=10, columnspan=4)
-ift1.grid(row=11, column=0, sticky='E')
-ift2.grid(row=11, column=1, sticky='E')
+mdltLabel.grid(row=12, columnspan=5)
+mdlt1.grid(row=13, column=0)
+mdlt2.grid(row=13, column=1)
+mdlt3.grid(row=13, column=2)
+mdlt4.grid(row=13, column=3)
 
+fsjtLabel.grid(row=14, columnspan=5)
+fsjt1.grid(row=15, column=0, columnspan=2)
+fsjt2.grid(row=15, column=3, columnspan=2)
 
+vstLabel.grid(row=16, columnspan=5)
+vst1.grid(row=17, column=0)
+vst2.grid(row=17, column=1)
+vst3.grid(row=17, column=2)
+
+#e = tkinter.Entry(tkinter)
+#e.grid(row=18, column=4)
+#Adding next buttont o go next frame for the options of each command
+b = tkinter.Button(text="Next", width=15)
+b.grid(row=18, columnspan=5)
 
 window.mainloop()                       #runs the tkinter window
