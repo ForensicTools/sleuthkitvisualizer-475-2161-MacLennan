b9a0a11a696baba48a4c1a7afe6ed52d705a2d22
Ryan MacLennan <galica9999@gmail.com>
2016-12-15 13:14:44 -0500

    Finished the command line version of the visualizer.  It requires that the python file be in the sleuthkit directory.  Specifically in the sluethkit one, not the /bin directory.  It already has the directory coded in so it is not necessary.  It takes in the command specified, the flags you need to run.  And will then continue appending all the commands into a list.  It will then run each command and create a txt file as the output.  The output files will be named after the command.

--- visualizer.py
+++ visualizer.py
@@ -10,32 +10,34 @@ import os
 
 
 #windows command path
-istat = "\\sleuthkit-4.3.0-win32\\bin\\istat.exe "
-fsstat = "\\sleuthkit-4.3.0-win32\\bin\\fsstat.exe "
-fls = "\\sleuthkit-4.3.0-win32\\bin\\fls.exe "
-ffind = "\\sleuthkit-4.3.0-win32\\bin\\ffind.exe "
-fcat = "\\sleuthkit-4.3.0-win32\\bin\\fcat.exe "
-icat = "\\sleuthkit-4.3.0-win32\\bin\\icat.exe "
-tsk_recover = "\\sleuthkit-4.3.0-win32\\bin\\tsk_recover.exe "
-tsk_loaddb = "\\sleuthkit-4.3.0-win32\\bin\\tsk_loaddb.exe "
-tsk_gettimes = "\\sleuthkit-4.3.0-win32\\bin\\tsk_gettimes.exe "
-tsk_comparedir = "\\sleuthkit-4.3.0-win32\\bin\\tsk_comparedir.exe "
-img_stat = "\\sleuthkit-4.3.0-win32\\bin\\img_stat.exe "
-img_cat = "\\sleuthkit-4.3.0-win32\\bin\\img_cat.exe "
-ils = "\\sleuthkit-4.3.0-win32\\bin\\ils.exe "
-ifind = "\\sleuthkit-4.3.0-win32\\bin\\ifind.exe "
-blkcat = "\\sleuthkit-4.3.0-win32\\bin\\blkcat.exe "
-blkls = "\\sleuthkit-4.3.0-win32\\bin\\blkls.exe "
-blkstat = "\\sleuthkit-4.3.0-win32\\bin\\blkstat.exe "
-blkcalc = "\\sleuthkit-4.3.0-win32\\bin\\blkcalc.exe "
-jcat = "\\sleuthkit-4.3.0-win32\\bin\\jcat.exe "
-jls = "\\sleuthkit-4.3.0-win32\\bin\\jls.exe "
-mmls = "\\sleuthkit-4.3.0-win32\\bin\\mmls.exe "
-mmcat = "\\sleuthkit-4.3.0-win32\\bin\\mmcat.exe "
-mmstat = "\\sleuthkit-4.3.0-win32\\bin\\mmstat.exe "
+istat = os.path.normpath("sleuthkit-4.3.0-win32/bin/istat.exe ")
+fsstat = os.path.normpath("sleuthkit-4.3.0-win32/bin/fsstat.exe ")
+fls = os.path.normpath("sleuthkit-4.3.0-win32/bin/fls.exe ")
+ffind = os.path.normpath("sleuthkit-4.3.0-win32/bin/ffind.exe ")
+fcat = os.path.normpath("sleuthkit-4.3.0-win32/bin/fcat.exe ")
+icat = os.path.normpath("sleuthkit-4.3.0-win32/bin/icat.exe ")
+tsk_recover = os.path.normpath("sleuthkit-4.3.0-win32/bin/tsk_recover.exe ")
+tsk_loaddb = os.path.normpath("sleuthkit-4.3.0-win32/bin/tsk_loaddb.exe ")
+tsk_gettimes = os.path.normpath("sleuthkit-4.3.0-win32/bin/tsk_gettimes.exe ")
+tsk_comparedir = os.path.normpath("/sleuthkit-4.3.0-win32/bin/tsk_comparedir.exe ")
+img_stat = os.path.normpath("sleuthkit-4.3.0-win32/bin/img_stat.exe ")
+img_cat = os.path.normpath("sleuthkit-4.3.0-win32/bin/img_cat.exe ")
+ils = os.path.normpath("sleuthkit-4.3.0-win32/bin/ils.exe ")
+ifind = os.path.normpath("sleuthkit-4.3.0-win32/bin/ifind.exe ")
+blkcat = os.path.normpath("sleuthkit-4.3.0-win32/bin/blkcat.exe ")
+blkls = os.path.normpath("sleuthkit-4.3.0-win32/bin/blkls.exe ")
+blkstat = os.path.normpath("sleuthkit-4.3.0-win32/bin/blkstat.exe ")
+blkcalc = os.path.normpath("sleuthkit-4.3.0-win32/bin/blkcalc.exe ")
+jcat = os.path.normpath("sleuthkit-4.3.0-win32/bin/jcat.exe ")
+jls = os.path.normpath("sleuthkit-4.3.0-win32/bin/jls.exe ")
+mmls = os.path.normpath("sleuthkit-4.3.0-win32/bin/mmls.exe ")
+mmcat = os.path.normpath("sleuthkit-4.3.0-win32/bin/mmcat.exe ")
+mmstat = os.path.normpath("sleuthkit-4.3.0-win32/bin/mmstat.exe ")
 
 commandList = [istat, fsstat, fls, ffind, fcat, icat, tsk_recover, tsk_loaddb, tsk_gettimes, tsk_comparedir, img_stat, img_cat, ils, ifind, blkcat, blkls, blkstat, blkcalc, jcat, jls, mmls, mmcat, mmstat]
+cwd = os.getcwd()
 
+#os.system(r"C:\\Users\\rxmhelp\\PycharmProjects\\untitled\\sleuthkit-4.3.0-win32\\bin\\fsstat.exe -f ntfs C:\\Users\\rxmhelp\\PycharmProjects\\untitled\\DiskPartitionRawImage.dd > fsstat.txt")
 
 print("Welcome to the sluethkit visualizer!")
 
@@ -45,24 +47,28 @@ if answer.lower() == "y":
         print("COOL")
         os.system("python3 gui.py")
 
-file = input("Please enter the file path of the image you want to run commands on: ")
-if '/' in file:
-    file = file.replace('/', '\\')
+file = os.path.normpath(input("Please enter the file path of the image you want to run commands on: "))
+
+#if '/' in file:
+#    file = file.replace('/', '\\')
 
 commandRunList = []
 
 while answer != 'y':
+    print(commandRunList)
     command = input("Please enter a command you would like to run (no .exe required): ")
     options = input("Please enter the flags to be used: ")
     for commands in commandList:
         if command in commands:
-            output = " > "+command+".txt"
+            output = os.path.normpath(" > "+ cwd + "\\"+ command + ".txt")
             commands = commands + options
+            commands = commands + " "  + file
             commands = commands + output
             commandRunList.append(commands)
             print(commandRunList)
-    answer = input("Would you like to run these commands now? (Y/N")
+    answer = input("Would you like to run these commands now? (Y/N)")
     answer = answer.lower()
 
 for run in commandRunList:
-        os.system(run)
+    print(run)
+    os.system(run)
